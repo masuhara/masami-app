f9d69387f069e1fdce0ab81ef36a168cfc4c639b
masuhara <info@masuhara.net>
2015-09-26 17:15:09 +0900

    Update TopPage

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,12 +2,15 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
   def show
     @project = Project.find(params[:id])
   end
+
   def new
     @project = Project.new
   end
+
   def create
     @project = Project.new(project_params)
     if @project.save
@@ -16,9 +19,11 @@ class ProjectsController < ApplicationController
       render 'new'
     end
   end
+
   def edit
     @project = Project.find(params[:id])
   end
+
   def update
     @project = Project.find(params[:id])
     if @project.update(project_params)
@@ -27,11 +32,13 @@ class ProjectsController < ApplicationController
       render 'edit'
     end
   end
+
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
     redirect_to projects_path
   end
+  
 private
   def project_params
     params[:project].permit(:title)
